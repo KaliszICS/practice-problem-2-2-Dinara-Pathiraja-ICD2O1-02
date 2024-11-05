c0f4e3ce3b40f78cba0b2ae9209c726790ce17c6
Dinara-Pathiraja-ICD2O1-02 <Dinara.pathiraja@student.tdsb.on.ca>
2024-11-04 22:53:12 -0500

    Update PP2_2.py

--- PP2_2.py
+++ PP2_2.py
@@ -1,16 +1,43 @@
 
 
+
 def q1(): 
-  #Write Assignment code here
+  integer = int(input ("Input an integer: "))
+  if(integer==5): 
+    print ("The number is Five")
+  else:
+    print("The number is not Five")
 
 
 def q2(): 
-  #Write Assignment code here
+  def q2(): 
+  number= float(input ("Input a number: "))
+  if(number>0):
+    print("Positive")
+  else: 
+    print ("Negative")
+
+def q3(): 
+  integer= int(input("Input an integer: "))
+  if (integer%2== 0):
+    print("Even")
+  else: 
+    print("Odd")
+
+def q4():
+  word= input('Type "Hello": ')
+  if(word== "Hello"):
+    print("The word is Hello")
+  else:
+    print("The word is not Hello")
 
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
+#q1()
+#q2()
+#q3()
+#q4()
+
